b56d895a1ad9562efb38608910da4c262beab75d
George-Lab204 <George.milad2@gmail.com>
2026-03-16 17:38:47 +0200

    fixError

--- Frontend/src/constants.ts
+++ Frontend/src/constants.ts
@@ -1,6 +1,7 @@
 export const BASE_URL = import.meta.env.DEV 
   ? "http://localhost:8000"  // your local backend port
-  : "https://tech-mart-theta.vercel.app"  // your deployed backendexport const PRODUCTS_URL = '/api/products'
+  : "https://tech-mart-theta.vercel.app"  // your deployed backend
+ export const PRODUCTS_URL = '/api/products'
 
 export const USERS_URL = '/api/users'
 export const ORDERS_URL = '/api/orders'
