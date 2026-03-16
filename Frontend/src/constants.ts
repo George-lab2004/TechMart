9129750cd6febad1cb7ce3f84dfabcb159a19541
George-Lab204 <George.milad2@gmail.com>
2026-03-16 17:37:20 +0200

    linkUpdate

--- Frontend/src/constants.ts
+++ Frontend/src/constants.ts
@@ -1,5 +1,6 @@
-export const BASE_URL = import.meta.env.DEV ? "http://localhost:5000" : ''
-export const PRODUCTS_URL = '/api/products'
+export const BASE_URL = import.meta.env.DEV 
+  ? "http://localhost:8000"  // your local backend port
+  : "https://tech-mart-theta.vercel.app"  // your deployed backendexport const PRODUCTS_URL = '/api/products'
 
 export const USERS_URL = '/api/users'
 export const ORDERS_URL = '/api/orders'
